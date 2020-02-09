c57b96c7b411b281904582cf90335cd32d20eae6
Travis Rempel <travis.rempel@wealthbar.com>
2020-02-08 22:45:54 -0800

    Use generated board state

--- js/index.vue.js
+++ js/index.vue.js
@@ -1,3 +1,98 @@
+//  -----------------------------
+//  -- RANDOM BOARD GENERATION --
+//  -----------------------------
+// Random generation is courtesy of https://github.com/tanglebones
+
+function randomCtor(seed) {
+  let current;
+
+  function* generator(n) {
+    current = current * 16807 % 2147483647;
+
+    const additionalSteps = (current & 0xF);
+    for(let i = 0; i <= additionalSteps; ++i) {
+      current = current * 16807 % 2147483647;
+    }
+
+    const r = (current - 1) / 2147483646;
+    yield Math.floor(r * n); // 0 to n-1
+  }
+
+  function init(seed) {
+    current = seed % 2147483647;
+    if (current <= 0) {
+      current += 2147483646;
+    }
+    const g = generator(1);
+    for (let i = 0; i < 128; ++i) { g.next(); }
+  }
+
+  init(seed);
+
+
+  generator.reseed = function (seed) {
+    init(seed);
+  };
+
+  return generator;
+}
+
+function shuffleCtor(random) {
+  return function shuffle(arr) {
+    for (let i = arr.length; i > 0; --i) {
+      const p = i - 1;
+      const j = random(i).next().value; // 0 to i-1
+      if (j === p) {
+        continue;
+      }
+      const t = arr[j];
+      arr[j] = arr[p];
+      arr[p] = t;
+    }
+    return arr;
+  }
+}
+
+function randomBoardCtor(shuffle) {
+  return function randomBoard() {
+    const s = shuffle("bbbbbbbbggggggggkkkkkkkkrrrrrrrrwwwwwwwwzzz".split(''));
+    for (const i of [42, 38, 31, 25, 18, 12, 5, 1]) {
+      s.splice(i, 0, '/');
+    }
+    return s.join('');
+  }
+}
+
+function stringToSeed(s) {
+  return s.split('').map(x => x.charCodeAt(0)).reduce((a, b) => (a * 16807 + b) % 2147483647, 1234567890);
+}
+
+function determineSeed(seedString) {
+  let seed = Math.trunc(Math.random() * 2147483647);
+
+  if (seedString) {
+    if ("" + +seedString === seedString) {
+      seed = +seedString;
+      console.log(`Using numeric seed: ${seed}`);
+    } else {
+      seed = stringToSeed(seedString);
+      console.log(`Using seed: ${seed} (from: "${seedString}")`);
+    }
+  } else {
+    console.log(`Using random seed: ${seed}`);
+  }
+  return seed;
+}
+
+// hard-coded seed for inital implementation
+// TODO: get a seed from Vue
+let seed = determineSeed('langk');
+
+const random = randomCtor(seed);
+const shuffle = shuffleCtor(random);
+const boardGenerator = randomBoardCtor(shuffle);
+
+
 const pieceDisplayPosition = (position, height) => {
   const [row, col] = [position[0], position[1]]
   const rows = {a:1, b:2, c:3, d:4, e:5, f:6, g:7, h:8, i:9}
@@ -112,18 +207,12 @@ const langk = new Vue({
   data: {
     // User inputs backed by localStorage
     input: {
-      boardInput: 'g/rrbr/wgzbwkg/kgkgkr/gwwbrww/bzrzkg/kwbwbbg/rkrk/b',
+      boardInput: '', // will be populated from random generator
       moveInput:
-`e3d2
-b1c2
-e1d1
-g7g6
-g5h4
-^g e6f6
-^b e5d5
-c6c7
--
-h3f5`,
+`e3d3
+^k c4d3
+^g f2d3
+^w g2d3`,
       p1: 'Alf',
       p2: 'Betty',
       colorScheme: 'rioGrande',
@@ -150,6 +239,8 @@ h3f5`,
     const localInput = JSON.parse(localStorage.getItem('input'))
     if (localInput) { this.input = localInput }
 
+    this.input.boardInput = boardGenerator();
+
     this.boardStartState = buildStartState(this.input.boardInput)
     this.buildHistory();
 
@@ -170,7 +261,7 @@ h3f5`,
     },
 
     updateInputs: function () {
-      // save work into localStorage
+      // save work into localStorage and update state to reflect the new input
       localStorage.setItem('input', JSON.stringify(this.input));
 
       this.boardStartState = buildStartState(this.input.boardInput)
@@ -196,6 +287,13 @@ h3f5`,
     goToEnd: function () {
       this.buildHistory();     
       this.gameState = hydrateBoard(this.boardStartState, this.moveHistory);
+    },
+
+    generateRandomBoard: function () {
+      seed = determineSeed(undefined);
+      this.input.boardInput = boardGenerator();
+      this.input.moveInput = '';
+      this.updateInputs();
     },
   },
 })
---
+++ js/random_board.node.js
@@ -0,0 +1,90 @@
+// this file courtesy of https://github.com/tanglebones
+
+function randomCtor(seed) {
+  let current;
+
+  function* generator(n) {
+    current = current * 16807 % 2147483647;
+
+    const additionalSteps = (current & 0xF);
+    for(let i = 0; i <= additionalSteps; ++i) {
+      current = current * 16807 % 2147483647;
+    }
+
+    const r = (current - 1) / 2147483646;
+    yield Math.floor(r * n); // 0 to n-1
+  }
+
+  function init(seed) {
+    current = seed % 2147483647;
+    if (current <= 0) {
+      current += 2147483646;
+    }
+    const g = generator(1);
+    for (let i = 0; i < 128; ++i) { g.next(); }
+  }
+
+  init(seed);
+
+
+  generator.reseed = function (seed) {
+    init(seed);
+  };
+
+  return generator;
+}
+
+function shuffleCtor(random) {
+  return function shuffle(arr) {
+    for (let i = arr.length; i > 0; --i) {
+      const p = i - 1;
+      const j = random(i).next().value; // 0 to i-1
+      if (j === p) {
+        continue;
+      }
+      const t = arr[j];
+      arr[j] = arr[p];
+      arr[p] = t;
+    }
+    return arr;
+  }
+}
+
+function randomBoardCtor(shuffle) {
+  return function randomBoard() {
+    const s = shuffle("bbbbbbbbggggggggkkkkkkkkrrrrrrrrwwwwwwwwzzz".split(''));
+    for (const i of [42, 38, 31, 25, 18, 12, 5, 1]) {
+      s.splice(i, 0, '/');
+    }
+    return s.join('');
+  }
+}
+
+function stringToSeed(s) {
+  return s.split('').map(x => x.charCodeAt(0)).reduce((a, b) => (a * 16807 + b) % 2147483647, 1234567890);
+}
+
+function determineSeed(seedString) {
+  let seed = Math.trunc(Math.random() * 2147483647);
+
+  if (seedString) {
+    if ("" + +seedString === seedString) {
+      seed = +seedString;
+      console.log(`Using numeric seed: ${seed}`);
+    } else {
+      seed = stringToSeed(seedString);
+      console.log(`Using seed: ${seed} (from: "${seedString}")`);
+    }
+  } else {
+    console.log(`Using random seed: ${seed}`);
+  }
+  return seed;
+}
+
+let seed = determineSeed(process && process.argv && (process.argv.length > 2) ? process.argv.slice(2).join(' ') : undefined);
+
+const random = randomCtor(seed);
+const shuffle = shuffleCtor(random);
+const boardGenerator = randomBoardCtor(shuffle);
+
+console.log(boardGenerator());
